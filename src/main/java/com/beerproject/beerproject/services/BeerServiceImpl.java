b164d4bd797a2c16431218fcb0806a6115dc5910
Raul Jansma Paes <rpjansma@gmail.com>
2021-01-16 17:10:56 -0300

    Put, Post and Delete methods implemented in Beer endpoint.

--- src/main/java/com/beerproject/beerproject/services/BeerServiceImpl.java
+++ src/main/java/com/beerproject/beerproject/services/BeerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.beerproject.beerproject.services;
 
 import com.beerproject.beerproject.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -14,4 +16,21 @@ public class BeerServiceImpl implements BeerService {
                 .beerType("Pale Ale")
                 .build();
     }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {}
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
+    }
+
+
 }
